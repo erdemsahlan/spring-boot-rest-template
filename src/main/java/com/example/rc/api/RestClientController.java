1468793370fb065c9491b397db23c4c1ca119610
Erdem Şahlan <sahlanerdem@gmail.com>
2021-04-21 17:12:47 +0300

    first commit

---
+++ src/main/java/com/example/rc/api/RestClientController.java
@@ -0,0 +1,38 @@
+package com.example.rc.api;
+
+import com.example.rc.Model.KisiDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/test")
+public class RestClientController {
+
+    private static final String webUrl = "http://localhost:8082/kisi";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping
+    public ResponseEntity<List<KisiDto>> getKisiList(){
+        ResponseEntity<List> result = restTemplate.getForEntity(webUrl, List.class);
+        List<KisiDto> responseBody=result.getBody();
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @PostMapping
+    public ResponseEntity<KisiDto> kaydet(@RequestBody KisiDto kisiDto){
+        ResponseEntity<KisiDto> result = restTemplate.postForEntity(webUrl, kisiDto, KisiDto.class);
+        KisiDto responseBody = result.getBody();
+        return ResponseEntity.ok(responseBody);
+    }
+
+}
